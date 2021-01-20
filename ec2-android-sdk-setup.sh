d98475b01212c5581ab50c0cbf14989c008afda5
Benoit Lubek <BoD@JRAF.org>
2021-01-20 14:36:30 +0100

    Update versions

--- ec2-android-sdk-setup.sh
+++ ec2-android-sdk-setup.sh
@@ -9,14 +9,14 @@ java -version
 
 # Download and unzip the Android command line tools
 # Latest version of this file available here: https://developer.android.com/studio/#command-tools
-curl --progress-bar --location https://dl.google.com/android/repository/commandlinetools-linux-6609375_latest.zip > cmdline-tools.zip
+curl --progress-bar --location https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip > cmdline-tools.zip
 unzip -qq cmdline-tools.zip -d cmdline-tools
 
 # Install Android components
 echo y | cmdline-tools/tools/bin/sdkmanager --sdk_root=android-sdk 'platform-tools' > /dev/null
 # Latest version of build-tools available here: https://developer.android.com/studio/releases/build-tools.html
 echo y | cmdline-tools/tools/bin/sdkmanager --sdk_root=android-sdk 'build-tools;29.0.3' > /dev/null
-echo y | cmdline-tools/tools/bin/sdkmanager --sdk_root=android-sdk 'platforms;android-29' > /dev/null
+echo y | cmdline-tools/tools/bin/sdkmanager --sdk_root=android-sdk 'platforms;android-30' > /dev/null
 
 # Cleanup
 rm -rf cmdline-tools.zip cmdline-tools
